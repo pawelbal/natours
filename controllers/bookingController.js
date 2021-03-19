796dbd1e62c22af2b1a53ace3602aa0340732a32
pawelbal <pawelbal@op.pl>
2021-03-19 22:57:11 +0100

    fixin bugs in stripes implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -22,7 +22,7 @@ exports.getCheckoutSession = catchAsync ( async (req, res, next) => {
             {
                 name: `${tour.name} Tour`,
                 description: tour.summary,
-                images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+                images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
                 amount: tour.price * 100,
                 currency: 'usd',
                 quantity: 1
@@ -51,11 +51,12 @@ exports.getCheckoutSession = catchAsync ( async (req, res, next) => {
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id
     const user = (await User.findOne({email: session.customer.email})).id
-    const price = session.line_items[0].amount / 100
+    const price = session.display_items[0].amount / 100
     await Booking.create({tour, user, price})
 }
 
 exports.webhookCheckout = (req, res, next) => {
+    const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
     const signature = req.headers['stripe-signature']
     let event
 
@@ -65,7 +66,7 @@ exports.webhookCheckout = (req, res, next) => {
         return res.status(400).send(`Webhook error: ${err.message}`)
     }
     
-    if(event.type === 'checkout.session.complete')
+    if(event.type === 'checkout.session.completed')
         createBookingCheckout(event.data.object)
 
     res.status(200).json({ received: true })
