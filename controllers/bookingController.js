6579c012b5240862d238d901e33a9d295e7f70a3
pawelbal <pawelbal@op.pl>
2021-03-19 23:45:22 +0100

    fixin bugs 3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -18,14 +18,19 @@ exports.getCheckoutSession = catchAsync ( async (req, res, next) => {
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
-        display_items: [
+        mode: 'payment',
+        line_items: [
             {
-                name: `${tour.name} Tour`,
-                description: tour.summary,
-                images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
-                amount: tour.price * 100,
-                currency: 'usd',
-                quantity: 1
+                quantity: 1,
+                price_data: {
+                    currency: 'usd',
+                    unit_amount: tour.price * 100,
+                    product_data: {
+                        name: `${tour.name} Tour`,
+                        description: tour.summary,
+                        images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+                    }
+                }
             }
         ]
     })
@@ -48,10 +53,10 @@ exports.getCheckoutSession = catchAsync ( async (req, res, next) => {
 //     res.redirect(req.originalUrl.split('?')[0])
 // })
 
-const createBookingCheckout = async session => {
+const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id
     const user = (await User.findOne({email: session.customer.email})).id
-    const price = session.display_items[0].amount / 100
+    const price = session.amount_total / 100
     await Booking.create({tour, user, price})
 }
 
@@ -66,8 +71,7 @@ exports.webhookCheckout = (req, res, next) => {
         return res.status(400).send(`Webhook error: ${err.message}`)
     }
     
-    if(event.type === 'checkout.session.completed')
-        createBookingCheckout(event.data.object)
+    if(event.type === 'checkout.session.completed') createBookingCheckout(event.data.object)
 
     res.status(200).json({ received: true })
 }
